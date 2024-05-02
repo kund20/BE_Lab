6a98fd76162001c53f1069d7e6cd53d1c236d582
kund20 <nguyentiendat6849@gmail.com>
2024-05-02 18:07:41 +0700

    migrate

--- database/seeders/FavoriteSeeder.php
+++ database/seeders/FavoriteSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class FavoriteSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class FavoriteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        DB::table('favorities')->insert([
+            'favorite_name' => 'Thể thao',
+            'favorite_description' => 'Thích câu lạc bộ Real, Barca, MU, ...',
+        ]);
+
+        DB::table('favorities')->insert([
+            'favorite_name' => 'Du lịch',
+            'favorite_description' => 'Tôi yêu Việt Nam, tối muốn đi du lịch khắp mọi miền đất nước, ...',
+        ]);
+
+        DB::table('favorities')->insert([
+            'favorite_name' => 'Xem phim',
+            'favorite_description' => 'Tôi rất thích xem phim hoạt hình',
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456'),
+            'phone' => '213456789',
+            'avatar' => 'test',
+            'mssv' => 'test'
+        ]);
     }
 }
